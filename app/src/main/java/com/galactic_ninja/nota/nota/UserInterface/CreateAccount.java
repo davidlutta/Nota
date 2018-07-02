d9080b3701f5e7516427c19cae4a0eaee9057212
davidlutta <davidlutta2010@gmail.com>
2018-07-02 15:51:32 +0300

    Add login feature and create account feature using firebase

--- app/src/main/java/com/galactic_ninja/nota/nota/UserInterface/CreateAccount.java
+++ app/src/main/java/com/galactic_ninja/nota/nota/UserInterface/CreateAccount.java
@@ -1,15 +1,126 @@
 package com.galactic_ninja.nota.nota.UserInterface;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.galactic_ninja.nota.nota.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-public class CreateAccount extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class CreateAccount extends AppCompatActivity implements View.OnClickListener{
+    @BindView(R.id.emailEditText) EditText mEmailEdittext;
+    @BindView(R.id.passwordEditText) EditText mPasswordEditText;
+    @BindView(R.id.signUpButton) CardView mSignUpButton;
+    @BindView(R.id.loginButton) CardView mLoginButton;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        ButterKnife.bind(this);
+        mSignUpButton.setOnClickListener(this);
+        mLoginButton.setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
+        createAuthStateListener();
     }
+
+    @Override
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.signUpButton:
+                createNewUser();
+                break;
+            case R.id.loginButton:
+                Intent intent = new Intent(CreateAccount.this, MainActivity.class);
+                startActivity(intent);
+        }
+    }
+
+    private void createNewUser(){
+        final String email = mEmailEdittext.getText().toString().trim();
+        final String password = mPasswordEditText.getText().toString().trim();
+
+
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        e.printStackTrace();
+                        Toast.makeText(CreateAccount.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(CreateAccount.this, "Account Created Successfully", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(CreateAccount.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void sendVerificationEmail() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        user.sendEmailVerification()
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(CreateAccount.this, "Email Sent", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void createAuthStateListener(){
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    sendVerificationEmail();
+                    Intent intent = new Intent(CreateAccount.this,Holder.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+    }
+
+
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        if (mAuthStateListener != null){
+            mAuth.removeAuthStateListener(mAuthStateListener);
+        }
+    }
+
+
 }
--- app/src/main/java/com/galactic_ninja/nota/nota/UserInterface/MainActivity.java
+++ app/src/main/java/com/galactic_ninja/nota/nota/UserInterface/MainActivity.java
@@ -1,13 +1,23 @@
 package com.galactic_ninja.nota.nota.UserInterface;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.galactic_ninja.nota.nota.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -15,6 +25,10 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     @BindView(R.id.loginButton) CardView mLoginButton;
     @BindView(R.id.createAccountButton) CardView mCreateAccountButton;
+    @BindView(R.id.emailEditText) EditText mEmail;
+    @BindView(R.id.passwordEditText) EditText mPassword;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +36,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         mCreateAccountButton.setOnClickListener(this);
+        mLoginButton.setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(MainActivity.this, Holder.class);
+                    startActivity(intent);
+                    finish();
+                    Toast.makeText(MainActivity.this, "Welcome", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
     }
 
     @Override
@@ -30,6 +59,53 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Intent intent = new Intent(MainActivity.this,CreateAccount.class);
             startActivity(intent);
         }
+
+        if (view == mLoginButton){
+            loginWithPassword();
+        }
+    }
+
+    private void loginWithPassword(){
+        String email = mEmail.getText().toString().trim();
+        String password = mPassword.getText().toString().trim();
+
+        if (email.equals("")){
+            mEmail.setError("Please Enter your Email");
+            return;
+        }
+
+        if (password.equals("")){
+            mPassword.setError("Please Enter your Password");
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Login Failed please try again", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()){
+                            Toast.makeText(MainActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+                        }
+                        Toast.makeText(MainActivity.this, "please verify Email", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        mAuth.addAuthStateListener(mAuthStateListener);
     }
 
 }
